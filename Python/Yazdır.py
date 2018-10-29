02c9130a28a79f51dad354786077a26a75e37e26
uyguns <mehmetuygun82@hotmail.com>
2018-10-29 21:19:54 +0300

    son değişiklik

--- Python/Yazdır.py
+++ Python/Yazdır.py
@@ -1,7 +1,22 @@
+print("Merhaba Dunya")
+#Print fonksiyonun gorevi verilen degerleri ekrana yazdirmaktir.
+#"Merhaba dünya"  ifadesi parametredir.
+#'Tek tirrnak isaretide kullanabiliriz.'
+print('Hosgeldin')
+print("Merhaba Dunya", 'Ben geldim')
+#Nesne İsleme
+a=5
+b=4
+c= "Pazardan"
+d= "Marketten"
+print(c, b, "Poset", d, a, "Poset")
 
-
-
-y=input("Yasiniz  Nedir? ")
-print("Demek", y, "yasındasiniz")
-
-
+#PRINT PARAMETRELERI
+#1= 'sep' Parametresi
+a="Fizik"
+b="Kimya"
+c="Biyoloji"
+print(a, b, c)
+print(a, b, c, sep='-')
+#Fizik-Kimya-Biyoloji   Sep sonrası tırnak iaretleri icine ne  koyarsan araya o gelir
+print(a,b,c,sep='/
