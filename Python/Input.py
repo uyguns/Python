479063b11ab475d2fd6ebbe2387896db28422517
uyguns <mehmetuygun82@hotmail.com>
2018-10-29 17:10:16 +0300

    değişiklik

--- Python/Input.py
+++ Python/Input.py
@@ -1,23 +1,4 @@
-#İnput Kullanıcadan veri alamyı sağlar
-input("Giris: ")
 
-#input fonksiyonu kullanıcıdan bir değer alıp
-#  bu değeri döndürür. Eğer biz bu fonksiyonu yukarıdaki gibi 
-# kullanırsak alınan veriyi bir daha kullanamayız. 
-# Aldığımız veriyi kullanmak için input fonksiyonunu bir 
-# nesneye atamalıyız.
-input("a' nın Degerı:  ")
 
-Giris= input("Lutfen Kullanici Adinizi Giriniz: ")
-print(Giris)
-
-Hosgeldiniz_Yazisi = """
-Hos Geldiniz...
-Bu program pyton diliyle konsol uzerinden yazilmistir. """
-print(Hosgeldiniz_Yazisi)
-
-K_Adi=input('Lutfen kullanici adini girin: ')
-print(K_Adi, 'Adli uye basariyle giris yaptı. ')
-
-x=input("Adinizi girin: ")
-print("merhaba, "  +x)
+isim=input("Adınızı girin:")
+print("Merhaba", isim, end="!\n")
