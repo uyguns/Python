c489cf0b138d92e7b7425bab9822cad48d4c3033
uyguns <mehmetuygun82@hotmail.com>
2018-10-29 14:26:22 +0300

    İkinci Calışma

---
+++ Python/Input.py
@@ -0,0 +1,20 @@
+#İnput Kullanıcadan veri alamyı sağlar
+input("Giris: ")
+
+#input fonksiyonu kullanıcıdan bir değer alıp
+#  bu değeri döndürür. Eğer biz bu fonksiyonu yukarıdaki gibi 
+# kullanırsak alınan veriyi bir daha kullanamayız. 
+# Aldığımız veriyi kullanmak için input fonksiyonunu bir 
+# nesneye atamalıyız.
+input("a' nın Degerı:  ")
+
+Giris= input("Lutfen Kullanici Adinizi Giriniz: ")
+print(Giris)
+
+Hosgeldiniz_Yazisi = """
+Hos Geldiniz...
+Bu program pyton diliyle konsol uzerinden yazilmistir. """
+print(Hosgeldiniz_Yazisi)
+
+K_Adi=input('Lutfen kullanici adini girin: ')
+print(K_Adi, 'Adli uye basariyle giris yaptı. ')
