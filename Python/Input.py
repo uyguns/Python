3fe8dbca09eea03ddd9ade39455bd4a2e816c485
uyguns <mehmetuygun82@hotmail.com>
2018-10-29 20:41:17 +0300

    değişiklik

--- Python/Input.py
+++ Python/Input.py
@@ -1,41 +1,9 @@
 
 
-#İnput Kullanıcadan veri alamyı sağlar
-input("Giris: ")
-
-#input fonksiyonu kullanıcıdan bir değer alıp
-#  bu değeri döndürür. Eğer biz bu fonksiyonu yukarıdaki gibi 
-# kullanırsak alınan veriyi bir daha kullanamayız. 
-# Aldığımız veriyi kullanmak için input fonksiyonunu bir 
-# nesneye atamalıyız.
-input("a' nın Degerı:  ")
-
-Giris= input("Lutfen Kullanici Adinizi Giriniz: ")
-print(Giris)
-
-Hosgeldiniz_Yazisi = """
-Hos Geldiniz...
-Bu program pyton diliyle konsol uzerinden yazilmistir. """
-print(Hosgeldiniz_Yazisi)
-
-K_Adi=input('Lutfen kullanici adini girin: ')
-print(K_Adi, 'Adli uye basariyle giris yaptı. ')
-
-x=input("Adinizi girin: ")
-print("merhaba, "  +x)
-
-isim=input("Adınızı girin:")
-print("Merhaba", isim, end="!\n")
-
-#Örnek -1 : Girilen iki adet sayıyı toplayan program yazınız
-ilk_sayi=input("1. sayiyi yaziniz: ")
-ikinci_sayi=input("2. sayyiyi yazınız: ")
-topla=int("ilk_sayi+ikinci_sayi")
-print(topla)
 
 #Dairenin çapını girmesini kullanıcıdan isteyelim.
 cap=input("Dairenin capini girin: ")
-yaricap=int(cap)/2
+yaricap=int(cap) /2
 pi=3.14159
-alan=pi*(yaricap*yaricap)
-print("capi", cap "cm olan dairenin alanı=", alan, "cm2'dir" )
+alan=pi(yaricap*yaricap)
+print("capi", cap, "cm olan dairenin alani:", alan, "cm2'dir" )
