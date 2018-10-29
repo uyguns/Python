84d9f9414fbc5abd8b6133ae1477d889e3fc459a
uyguns <mehmetuygun82@hotmail.com>
2018-10-29 20:57:03 +0300

    cc

--- Python/Input.py
+++ Python/Input.py
@@ -1,9 +1,3 @@
 
 
 
-#Dairenin çapını girmesini kullanıcıdan isteyelim.
-cap=input("Dairenin capini girin: ")
-yaricap=int(cap) /2
-pi=3.14159
-alan=pi(yaricap*yaricap)
-print("capi", cap, "cm olan dairenin alani:", alan, "cm2'dir" )
