281d2d61d2a6c35417f2e25f07d054713bcdff80
uygun <mehmetuygun82@hotmail.com>
2018-11-03 02:09:59 +0300

    a

--- Kacis.py
+++ Kacis.py
@@ -1,21 +1,3 @@
 
-#Kacis dizeleri 
-#Ters Taksim "\" Asagida tek kesme isaretinin yerini  belirler.
-print('Yarin adana \'ya gidiyorum')
-print("Python programlama dili adini \"python\" yilanindan almaz. ")
-
-
-#Satir Basi \n 
-#En temel kacis dizesidir.Satir basikarakteridir.
-print("Birinci satir\nIkinci satir\nUcuncu satir")
-#Birinci satir
-#Ikinci satir
-#Ucuncu satir   /Gorunum bu hale geliyor.
 Baslik = "Python Programlama Dili"
 print(Baslik, "\n", "-"*len(Baslik), sep="")
-#\n dizisi sayesinde ilk parametre(Baslik yani "python programlama dili" ekrana yazdirildiktan sonra alt dizine gecer.
-#"-"*len(Baslik) degiskenin altina cizgi ceker.
-#len parametresi degiskenin uzunlugu adedince cizgi cizer
-#sep parametresi cizgilerin bas tarafina bosluk ekler.
-# Biz bu kod ile bosluk karakteri yerine bos bir karakter
-#dizesi yerlestiriyoruz.
