5db8d1c110867826fcc4c60bc4f6015047477967
uygun <mehmetuygun82@hotmail.com>
2018-11-03 01:28:01 +0300

    a

--- Kacis.py
+++ Kacis.py
@@ -1,3 +1,4 @@
 #Kacis dizeleri 
 #Ters Taksim "\" Asagida tek kesme isaretinin yerini  belirler.
 print('Yarin adana \'ya gidiyorum')
+print("Python programlama dili adini \"python\" yilanindan almaz. ")
--- int.py
+++ int.py
@@ -2,6 +2,21 @@
 
 
 
+#int karakter dizelerini  sayiya cevirir
+v1=("23")
+sayi=int(v1)
+print(3.99 *  sayi)
+
+#str  sayilari karakter dizelerine cevirir
+a=23
+car=str(23)
+print(car)
+#int tamsayidir   
+#1245  gibi
+# #float ondalik sayilari gösterir
+x= 23.88
+print(type(x))
+
 #Complex  sayilar harf ve sayi karisimidir
 a= 5j
 print(type(a))
