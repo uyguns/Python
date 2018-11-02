e7a2b777a2c92a9421c8de7bb0503c349d3c583f
uygun <mehmetuygun82@hotmail.com>
2018-11-03 02:43:10 +0300

    a

--- Kacis.py
+++ Kacis.py
@@ -1,4 +1,33 @@
+#Kacis dizeleri 
+#Ters Taksim "\" Asagida tek kesme isaretinin yerini  belirler.
+print('Yarin adana \'ya gidiyorum')
+print("Python programlama dili adini \"python\" yilanindan almaz. ")
+
+
+#Satir Basi \n 
+#En temel kacis dizesidir.Satir basikarakteridir.
+print("Birinci satir\nIkinci satir\nUcuncu satir")
+#Birinci satir
+#Ikinci satir
+#Ucuncu satir   /Gorunum bu hale geliyor.
 
 Baslik="Python Programlama Dili"
-print(Baslik, "\n", "-"*len(Baslik))
+print(Baslik, "\n", "-"*len(Baslik), sep="")
+#\n dizisi sayesinde ilk parametre(Baslik yani "python programlama dili" ekrana yazdirildiktan sonra alt dizine gecer.
+#"-"*len(Baslik) degiskenin altina cizgi ceker.
+#len parametresi degiskenin uzunlugu adedince cizgi cizer
+#sep parametresi cizgilerin bas tarafina bosluk ekler.
+# Biz bu kod ile bosluk karakteri yerine bos bir karakter
+#dizesi yerlestiriyoruz.
 
+print("c:\nisan\uygun.txt")
+#python  c: den sonraki "\n"isan  karakterini satır atlama olarak anladi.
+Cozum 
+print(C:\\nisan
+
+
+print(*"123456789", sep="\t")
+
+#Cikti 1	2	3	4	5	6	7	8	9
+
+#\a Parametresi
