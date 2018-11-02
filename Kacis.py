21580fc0aee328254ca3a6e704b87cd5fb775ad6
uygun <mehmetuygun82@hotmail.com>
2018-11-03 01:42:19 +0300

    a

--- Kacis.py
+++ Kacis.py
@@ -1,8 +1,13 @@
 
+#Kacis dizeleri 
+#Ters Taksim "\" Asagida tek kesme isaretinin yerini  belirler.
+print('Yarin adana \'ya gidiyorum')
+print("Python programlama dili adini \"python\" yilanindan almaz. ")
+
 
 #Satir Basi \n 
 #En temel kacis dizesidir.Satir basikarakteridir.
 print("Birinci satir\nIkinci satir\nUcuncu satir")
 #Birinci satir
 #Ikinci satir
-#Ucuncu satir   /Gorunum bu hale geliyor.
+#Ucuncu satir   /Gorunum bu hale geliyor
