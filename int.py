49230742a242d18fd5637313ae74653d92e12337
uyguns <44535625+uyguns@users.noreply.github.com>
2018-11-02 17:57:07 +0300

    Add files via upload

--- int.py
+++ int.py
@@ -1,4 +1,22 @@
+
+
+#int karakterdizelerini  sayıya cevirir
 v1=("23")
 sayi=int(v1)
 print(3.99 *  sayi)
 
+#str  sayılarıkarakter dizelerine cevirir
+a=23
+car=str(23)
+print(car)
+#int tamsayıdır   
+#1245  gibi
+# #float ondalık sayılarıgösterir
+x= 23.88
+print(type(x))
+
+
+
+#Complex  sayılar harf ve sayıkarışımıdır
+a= 5j
+print(type(a))
